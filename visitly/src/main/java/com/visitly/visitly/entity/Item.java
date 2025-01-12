ddafb96f48a940fe99a8ec78e02fc5c805280dcb
Vikrant Yadav <vikrantmani45@gmail.com>
2025-01-12 18:08:28 +0530

    setting the entity

--- visitly/src/main/java/com/visitly/visitly/entity/Item.java
+++ visitly/src/main/java/com/visitly/visitly/entity/Item.java
@@ -1,5 +1,21 @@
 package com.visitly.visitly.entity;
 
-public class Item {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "items")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Item {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String name;
+  private String description;
+  private double price;
 }
