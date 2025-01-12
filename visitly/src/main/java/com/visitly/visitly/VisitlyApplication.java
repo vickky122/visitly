bf413973f62df80114740c989c96698fbf83777b
Vikrant Yadav <vikrantmani45@gmail.com>
2025-01-12 17:42:55 +0530

    first commit

---
+++ visitly/src/main/java/com/visitly/visitly/VisitlyApplication.java
@@ -0,0 +1,13 @@
+package com.visitly.visitly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisitlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisitlyApplication.class, args);
+	}
+
+}
