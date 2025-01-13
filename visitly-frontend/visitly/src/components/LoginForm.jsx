be3e7a9cdaf829868ad939eaef835de550f51f4c
Vikrant Yadav <vikrantmani45@gmail.com>
2025-01-13 21:43:05 +0530

    private route and login form

---
+++ visitly-frontend/visitly/src/components/LoginForm.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import authService from "../services/api";
+
+const LoginForm = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const data = await authService.login(username, password);
+      alert("Login Successful");
+    } catch (error) {
+      alert("Login Failed");
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        placeholder="Username"
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+      />
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+export default LoginForm;
