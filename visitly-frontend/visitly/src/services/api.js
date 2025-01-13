2aa58ed7ff8b0208b427d18f4909dc3acb38f050
Vikrant Yadav <vikrantmani45@gmail.com>
2025-01-13 22:09:59 +0530

    api update

--- visitly-frontend/visitly/src/services/api.js
+++ visitly-frontend/visitly/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const API_BASE_URL = 'http://localhost:8090/api/items';
+const API_AUTH_URL = 'http://localhost:8090/api/auth/login'; 
 
 export const getItems = async () => {
   const response = await axios.get(API_BASE_URL);
@@ -18,6 +19,17 @@ export const deleteItem = async (id) => {
     return response.data;
   } catch (error) {
     console.error("Error deleting item:", error);
+    throw error;
+  }
+};
+
+// Function to authenticate user and get JWT token
+export const login = async (username, password) => {
+  try {
+    const response = await axios.post(API_AUTH_URL, { username, password });
+    return response.data;  
+  } catch (error) {
+    console.error('Error logging in:', error);
     throw error;
   }
 };
