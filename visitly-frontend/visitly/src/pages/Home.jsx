4e5c9a53a023855b684a9fe60cde7d1e8890fcd2
Vikrant Yadav <vikrantmani45@gmail.com>
2025-01-13 13:26:18 +0530

    adding refresh functionality instantly

--- visitly-frontend/visitly/src/pages/Home.jsx
+++ visitly-frontend/visitly/src/pages/Home.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ItemList from '../components/ItemList';
 import AddItemForm from '../components/AddItemForm';
 
-const Home = () => (
-  <div>
-    <h1>Welcome to Visitly</h1>
-    <AddItemForm />
-    <ItemList />
-  </div>
-);
+const Home = () => {
+  const [refresh, setRefresh] = useState(false);
+
+  const refreshItems = () => {
+    setRefresh(!refresh);
+  };
+
+  return (
+    <div>
+      <h1>Welcome to Visitly</h1>
+      <AddItemForm onItemAdded={refreshItems} />
+      <ItemList refresh={refresh} />
+    </div>
+  );
+};
 
 export default Home;
