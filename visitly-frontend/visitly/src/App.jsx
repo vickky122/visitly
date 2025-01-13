2a5598d448df5649c70242dade6ec92c9d3313a8
Vikrant Yadav <vikrantmani45@gmail.com>
2025-01-13 21:45:33 +0530

    updating app

--- visitly-frontend/visitly/src/App.jsx
+++ visitly-frontend/visitly/src/App.jsx
@@ -4,12 +4,15 @@ import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
 import Footer from './components/Footer';
+import LoginForm from './components/LoginForm';
+import PrivateRoute from './components/PrivateRoute'; 
 
 const App = () => (
   <Router>
     <Navbar />
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<LoginForm />} /> 
+      <PrivateRoute path="/home" element={<Home />} /> 
       <Route path="/about" element={<About />} />
     </Routes>
     <Footer />
